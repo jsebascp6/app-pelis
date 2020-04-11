bdf3837b1ba7b68d601eb35955dc70f4b87d3c93
Sebastian Castro <jsebascp6@gmail.com>
2020-04-11 01:38:09 -0500

    init styles

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import Films from './Films'
 import Reserves from './Reserves'
+import './App.scss'
+import { CalendarOutlined, VideoCameraOutlined } from '@ant-design/icons'
 
 class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => <Films/>} />
-          <Route path="/Reservas" render={() => <Reserves/>} />
-        </Switch>
+        <div className='container'>
+          <div className='menu'>
+            <Link to="/">
+              <VideoCameraOutlined />
+              <span>Peliculas</span>
+            </Link>
+            <Link to="/Reservas">
+              <CalendarOutlined />
+              <span>Reservas</span>
+            </Link>
+          </div>
+          <div className='content'>
+            <Switch>
+              <Route exact path="/" render={() => <Films/>} />
+              <Route path="/Reservas" render={() => <Reserves/>} />
+            </Switch>
+          </div>
+        </div>
       </BrowserRouter>
     );
   }
